33184b2b3f43702cba37561199f9ef63aeb2f102
yuan <bb0981006376@gmail.com>
2024-11-26 02:08:53 +0800

    商品交易&amp;道具功能的程式

--- Assets/Scripts/Shop/shop.cs
+++ Assets/Scripts/Shop/shop.cs
@@ -7,6 +7,10 @@ public class shop : MonoBehaviour
    public GameObject ShopPanel;
     private bool nearby=false;
 
+    //public GameObject coinprefab;
+
+    public int coin = 1000; //FOR TEST
+
     // Start is called before the first frame update
     void Start()
     {
@@ -37,4 +41,93 @@ public class shop : MonoBehaviour
             ShopPanel.SetActive(true);
         }
     }
+    public void Restoration_Potion()
+    {
+        // if (playerHealth != null)
+        {
+            // playerHealth.AddHealth(6);
+        }
+    }
+    public void Buy_Restoration_Potion()
+    {
+        int cost = 50;
+        if (coin >= cost)
+        {
+            coin -= cost;
+            Restoration_Potion(); 
+            Debug.Log("購買成功！");
+        }
+        else
+        {
+            Debug.Log("金幣不足！");
+        }
+    }
+    private IEnumerator Stamina_Charm()
+    {
+        while (true)                    
+        {
+            yield return new WaitForSeconds(5);
+            //playerHealth.AddStrength(2); 
+        }
+    }
+    public void Buy_stamina_charm()
+    {
+        int cost = 300;
+        if (coin >= cost)
+        {
+            coin -= cost;
+            Stamina_Charm();
+            Debug.Log("購買成功！");
+        }
+        else
+        {
+            Debug.Log("金幣不足！");
+        }
+    }
+    private IEnumerator Source_Of_Life()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(10); 
+            //playerHealth.AddHealth(1);
+        }
+    }
+    public void Buy_Source_Of_Life()
+    {
+        int cost = 300;
+        if (coin >= cost)
+        {
+            coin -= cost;
+            Source_Of_Life();
+            Debug.Log("購買成功！");
+        }
+        else
+        {
+            Debug.Log("金幣不足！");
+        }
+
+    }
+    public void Death_Free_Gold_Medal()
+    {
+        //  if (playerHealth != null && playerHealth.isDead)
+        {
+            // playerHealth.Revive();  要寫一個總血量的1/3
+
+        }
+    }
+    public void Buy_Death_Free_Gold_Medal()
+    {
+        int cost = 500;
+        if (coin >= cost)
+        {
+            coin -= cost;
+            Death_Free_Gold_Medal();
+            Debug.Log("購買成功！");
+        }
+        else
+        {
+            Debug.Log("金幣不足！");
+        }
+    }
+
 }
