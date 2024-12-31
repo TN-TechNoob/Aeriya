24e8f40cd4a2aa888768c853747ed59f0250046b
TN_TechNoob <tn.technoob@gmail.com>
2024-12-31 21:13:31 +0800

    Revert "Update PlayerHealth.cs"
    This reverts commit ce9b4a9b0530d85622e2366c88c369a70443f4ef.

--- Assets/Scripts/Player/PlayerHealth.cs
+++ Assets/Scripts/Player/PlayerHealth.cs
@@ -6,17 +6,74 @@ public class PlayerHealth : MonoBehaviour
 {
     public Animator animator;
     public int maxHealth = 100;
+    public int maxStrength = 100;
     int currentHealth;
+<<<<<<< Updated upstream
+=======
+    public HealthBar_V2 healthbar;
+>>>>>>> Stashed changes
+    int currentStrength;
 
     void Start()
     {
         currentHealth = maxHealth;
+        healthbar.SetMaxHealth(maxHealth);
+    }
+
+    public void AddHealth(int amount)
+    {
+        currentHealth += amount;
+
+        currentHealth = Mathf.Clamp(currentHealth, 0, maxHealth);
+    }
+    public void AddStrength(int amount)
+    {
+        currentStrength += amount;
+
+        currentStrength = Mathf.Clamp(currentStrength, 0, maxStrength);
+    }
+
+    public bool isDead => currentHealth <= 0; 
+
+    public void Revive()
+    {
+        if (isDead)
+        {
+            currentHealth = maxHealth / 3; 
+           
+        }
+    }
+
+    public void AddHealth(int amount)
+    {
+        currentHealth += amount;
+
+        currentHealth = Mathf.Clamp(currentHealth, 0, maxHealth);
+    }
+    public void AddStrength(int amount)
+    {
+        currentStrength += amount;
+
+        currentStrength = Mathf.Clamp(currentStrength, 0, maxStrength);
+    }
+
+    public bool isDead => currentHealth <= 0; 
+
+    public void Revive()
+    {
+        if (isDead)
+        {
+            currentHealth = maxHealth / 3; 
+           
+        }
     }
 
     // Start is called before the first frame update
     public void TakeDamage(int damage)
     {
         currentHealth -= damage;
+
+        healthbar.Sethealth(currentHealth);
 
         animator.SetTrigger("Hurt");
         if (currentHealth <= 0)
