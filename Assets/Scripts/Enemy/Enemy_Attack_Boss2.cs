6ed1d312c34b4e015bf039616d1200df0c78a7ef
TN_TechNoob <tn.technoob@gmail.com>
2025-01-04 06:21:54 +0800

    獨立Boss攻擊程式

---
+++ Assets/Scripts/Enemy/Enemy_Attack_Boss2.cs
@@ -0,0 +1,78 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy_Attack_Boss2 : MonoBehaviour
+{
+    public Enemy_V2 enemy_V2;
+    public Animator animator;
+    public GameObject player;
+    private float distance;
+    public float attackRange;
+    public bool isAttacking;
+    public bool isAttack_V1;
+    public float attackCoolDown;
+    private float attackCD;
+    public int attackDamage;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        attackCD = attackCoolDown;
+    }
+
+    void Update()
+    {
+        if (enemy_V2.distance > attackRange && !isAttacking)
+        {
+            enemy_V2.Chase();
+        }
+        else if (distance <= attackRange && !isAttacking)
+        {
+            AttackPlayerCheck();
+        }
+    }
+
+    public void AttackPlayerCheck()
+    {
+        attackCD -= Time.deltaTime;
+        Debug.Log(distance + " " + isAttacking + " " + isAttack_V1);
+        enemy_V2.speed = 1.5f;
+        int attaackSelect = Random.Range(1, 3);
+        if (!isAttack_V1)
+        {
+            Debug.Log(attaackSelect);
+            if (attaackSelect == 1 && attackCD <= 0)
+            {
+                animator.SetTrigger("Attack_V1");
+                attackCD = attackCoolDown;
+            }
+            else if (attaackSelect == 2 && attackCD <= 0)
+            {
+                animator.SetTrigger("Attack_V2");
+                attackCD = attackCoolDown;
+            }
+        }
+        else
+        {   
+            animator.SetTrigger("Attack_V3");
+            attackCD = attackCoolDown;
+        }
+    }
+
+    public void AttackPlayer()
+    {
+        if (distance <= attackRange)
+        {
+            player.GetComponent<PlayerHealth>().TakeDamage(attackDamage);
+        }
+    }
+
+    public void BoostAndAttackPlayer()
+    {
+        if (distance > 4)
+        {
+            enemy_V2.speed = enemy_V2.speed * 2;
+        }
+    }
+}
--- Assets/Scripts/Enemy/Enemy_V2.cs
+++ Assets/Scripts/Enemy/Enemy_V2.cs
@@ -10,14 +10,8 @@ public class Enemy_V2 : MonoBehaviour
     public Animator animator;
     public GameObject player;
     public float speed;
-    private float distance;
-    public float attackRange;
-    public bool isAttacking;
-    public bool isAttack_V1;
-    public float attackCoolDown;
-    private float attackCD;
-    public int attackDamage = 20;
-    public int maxHealth = 100;
+    public float distance;
+    public int maxHealth;
     int currentHealth;
     public HealthBar_V2 healthbar;
 
@@ -25,60 +19,12 @@ public class Enemy_V2 : MonoBehaviour
     void Start()
     {
         currentHealth = maxHealth;
-        attackCD = attackCoolDown;
         healthbar.SetMaxHealth(maxHealth);
     }
 
     void Update()
     {
         CalculateDistance();
-
-        if (distance > attackRange && !isAttacking)
-        {
-            Chase();
-        }
-        else if (distance <= attackRange && !isAttacking)
-        {
-            attackCD -= Time.deltaTime;
-            Debug.Log(distance + " " + isAttacking + " " + isAttack_V1);
-            speed = 1.5f;
-            int attaackSelect = Random.Range(1, 3);
-            if (!isAttack_V1)
-            {
-                Debug.Log(attaackSelect);
-                if (attaackSelect == 1 && attackCD <= 0)
-                {
-                    animator.SetTrigger("Attack_V1");
-                    attackCD = attackCoolDown;
-                }
-                else if (attaackSelect == 2 && attackCD <= 0)
-                {
-                    animator.SetTrigger("Attack_V2");
-                    attackCD = attackCoolDown;
-                }
-            }
-            else
-            {   
-                animator.SetTrigger("Attack_V3");
-                attackCD = attackCoolDown;
-            }
-        }
-    }
-
-    public void AttackPlayer()
-    {
-        if (distance <= attackRange)
-        {
-            player.GetComponent<PlayerHealth>().TakeDamage(attackDamage);
-        }
-    }
-
-    public void BoostAndAttackPlayer()
-    {
-        if (distance > 4)
-        {
-            speed = speed * 2;
-        }
     }
 
     public void TakeDamage(int damage)
@@ -111,7 +57,7 @@ public class Enemy_V2 : MonoBehaviour
         direction.Normalize();
     }
 
-    void Chase()
+    public void Chase()
     {
         transform.position = Vector2.MoveTowards(this.transform.position, player.transform.position, speed * Time.deltaTime);
     }
